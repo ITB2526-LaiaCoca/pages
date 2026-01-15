9d73479564102f5b18142fb483f8a0c9aa7877b9
Laia Coca Carmona <laia.coca.7e9@itb.cat>
2026-01-15 06:01:13 +0000

    reterthy

--- js/jsProject.js
+++ js/jsProject.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   /* ENTRADA SUAVE AL CARGAR */
   const allBlocks = document.querySelectorAll(
-    "header, .subtitulo, .menu-lateral, .explanation, .imgs, footer"
+    "header, .subtitulo, .navegador, .explanation, .imgs, footer"
   );
 
   allBlocks.forEach((block, index) => {
